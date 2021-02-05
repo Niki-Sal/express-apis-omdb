855e280a952bfee058b488e3c19328e76f33dfb8
niki-sal <ne.salehi@gmail.com>
2021-02-05 15:07:33 -0800

    add database and favorite post route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const axios = require('axios')
 const app = express();
+const db = require('./models')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,8 +19,75 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  // res.send('Hello, backend!');
+  //send to homepage
+  res.render('index')
 });
+
+// route to handle query from search form
+app.get('/results', (req, res)=> {
+  let q = req.query.q
+  var qs = {
+    params: {
+      //s comes from imdb documentation (search on google)
+      s: q,
+      apikey: process.env.API_KEY
+    }
+  };
+  axios.get('http://www.omdbapi.com', qs)
+    .then(function (response) {
+      // handle success, carefule to see how the data comes back
+      let data = response.data.Search
+      res.render('results', {data})
+    })
+})
+
+// Make a new route on your backend: GET /movies/:movie_id
+app.get('/movies/:movie_id', (req, res)=> {
+  // new call to axios to get specific information on a movie
+  let movie_id = req.params.movie_id
+  var qs = {
+    params: {
+      //i comes from imdb documentation (search on google)
+      i: movie_id,
+      apikey: process.env.API_KEY
+    }
+  };
+  axios.get('http://www.omdbapi.com', qs)
+    .then(function (response) {
+      // handle success, carefule to see how the data comes back
+      let data = response.data
+      console.log(data)
+      res.render('detail', {data})
+    })
+})
+
+
+//POST routes to /faves from the favorite button
+
+// post route to /faves from the favorite button
+app.post('/faves', (req, res)=> {
+  // with a post from a form we will have acess on req.body
+  const favMovie = req.body
+  // checks if this movie exists, if it is not it will create,
+  // if it is it will return it
+  db.favorite.findOrCreate({
+    where: {
+      imdbid: favMovie.imdbid,
+      title: favMovie.title
+    }
+  })
+  .then(([movie, didCreate])=> {
+    if(didCreate) {
+      console.log(movie.get())
+    }
+    res.redirect('/')
+  })
+  .catch( (err)=> {
+    console.log(err)
+  })
+})
+
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
